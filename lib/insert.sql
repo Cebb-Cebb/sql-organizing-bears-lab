a32429efc651193fae2d319261aa01efe583c8d9
Propioception <github email address>
2020-12-17 13:55:29 -0500

    commit tree e6b3bfa464d2949ab45abc91cc8eb665f8e53602

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Mr. Chocolate", 2, "F", "Brown", "Chills", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Rowdy", 4, "M", "Brown", "goofy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Tabitha", 5, "M", "Brown", "quiet", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Sergeant Brown", 6, "F", "Brown", "hyper", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Melissa", 5, "F", "Black", "playful", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Grinch", 8, "M", "Green", "angry", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Wendy", 10, "F", "Brown", "laid back", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES(NULL, NULL, NULL, NULL, NULL, NULL);
